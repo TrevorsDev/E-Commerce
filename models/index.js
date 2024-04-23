b7f21eadb0accc9ea508e0a26dd3df849cc5a8a6
Trevor Johnson <trevor9j@protonmail.com>
2024-04-22 21:59:48 -0700

    added models

--- models/index.js
+++ models/index.js
@@ -5,8 +5,17 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
+
+// Products have many Tags (through ProductTag)
 
 // Products belongToMany Tags (through ProductTag)
 
